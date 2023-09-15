8ad280b4c206437547460191097a9624c48a06f6
Ellie <yokomone@yahoo.com>
2023-09-16 02:34:23 +0300

    made hamburger menu animated

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,19 @@
 let menu_button = document.getElementById('hamburger_menu')
 let navbar_buttons = document.getElementById('navbar_buttons')
+let header = document.getElementById('header')
+let on = 0
 
-menu_button.addEventListener('click', () => {
-    menu_button.style.display = 'none'
-    navbar_buttons.style.display = 'flex'
+menu_button.parentElement.addEventListener('click', () => {
+    if (on) {
+        navbar_buttons.style.height = '0'
+        navbar_buttons.style.marginTop = '0'
+        header.style.alignItems = 'center'
+        on = 0
+    }
+    else {
+        navbar_buttons.style.height = '25vh'
+        navbar_buttons.style.marginTop = '2vh'
+        header.style.alignItems = 'baseline'
+        on = 1
+    }
 })
-
-console.log('wow')
