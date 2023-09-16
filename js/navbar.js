127a5a006a7ae90c55a505d3ae0510b569fa7309
Ellie <yokomone@yahoo.com>
2023-09-16 03:02:22 +0300

    fixed animation issue with hamburger menu

--- js/navbar.js
+++ js/navbar.js
@@ -6,14 +6,16 @@ let on = 0
 menu_button.parentElement.addEventListener('click', () => {
     if (on) {
         navbar_buttons.style.height = '0'
+        navbar_buttons.style.width = '0'
+        navbar_buttons.style.margin = '0'
         navbar_buttons.style.marginTop = '0'
-        header.style.alignItems = 'center'
         on = 0
     }
     else {
         navbar_buttons.style.height = '25vh'
         navbar_buttons.style.marginTop = '2vh'
-        header.style.alignItems = 'baseline'
+        navbar_buttons.style.width = '95vw'
+        navbar_buttons.style.margin = '0'
         on = 1
     }
 })
