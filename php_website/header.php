b440d3f34c435e19a7e204ae1bada07142daff94
Nikolay St. Dimitrov <126271396+nikolay-st-d@users.noreply.github.com>
2023-09-19 13:20:24 +0300

    header

--- php_website/header.php
+++ php_website/header.php
@@ -1,21 +1,41 @@
-<header id="header">
-        <div class="navbar_logo"><a href="/new/"><img src="images/pinkovilogo-bg.png"></a></div>
-        <div id="navbar_buttons" class="navbar_buttons">
-            <a href="index.php#about"><button>ЗА ХОТЕЛА</button></a>
-            <a href="index.php#bistro"><button>БИСТРО</button></a>
-            <a href="index.php#entertainment"><button>ЗАБАВЛЕНИЯ</button></a>
-            <a href="index.php#location"><button>ЛОКАЦИЯ</button></a>
-            <a href="gallery.php"><button>ГАЛЕРИЯ</button></a>
-            <a href="contacts.php"><button>КОНТАКТИ</button></a>
-        </div>
-        <div class="right">
-            <div class="hamburger_menu"><i id="hamburger_menu" class="fa-solid fa-bars"></i></div>
-            <div class="lang_and_book">
-                <div class="navbar_languages">
-                    <a href="#"><img src="images/bulgarian.png"></a>
-                    <a href="#"><img src="images/english.png"></a>
-                </div>
-                <div class="navbar_book"><a href="prices.php"><button>РЕЗЕРВИРАЙ</button></a></div>
-            </div>
-        </div>
+<header id="header">
+
+        <div class="navbar_logo"><a href="/new/"><img src="images/pinkovilogo-bg.png"></a></div>
+
+        <div id="navbar_buttons" class="navbar_buttons">
+
+            <a href="index.php#about"><button>ЗА ХОТЕЛА</button></a>
+
+            <a href="index.php#bistro"><button>БИСТРО</button></a>
+
+            <a href="index.php#entertainment"><button>ЗАБАВЛЕНИЯ</button></a>
+
+            <a href="index.php#location"><button>ЛОКАЦИЯ</button></a>
+
+            <a href="gallery.php"><button>ГАЛЕРИЯ</button></a>
+
+            <a href="contacts.php"><button>КОНТАКТИ</button></a>
+
+        </div>
+
+        <div class="right">
+
+            <div class="hamburger_menu"><i id="hamburger_menu" class="fa-solid fa-bars"></i></div>
+
+            <div class="lang_and_book">
+
+                <div class="navbar_languages">
+
+                    <a href="index.php"><img src="images/bulgarian.png"></a>
+
+                    <a href="index_en.php"><img src="images/english.png"></a>
+
+                </div>
+
+                <div class="navbar_book"><a href="prices.php"><button>РЕЗЕРВИРАЙ</button></a></div>
+
+            </div>
+
+        </div>
+
     </header>
