936b47ba954e399de345f2ddb4733dc7639bf569
Nikolay St. Dimitrov <126271396+nikolay-st-d@users.noreply.github.com>
2023-09-19 12:11:37 +0300

    file names adjusted

--- php_website/index_en.php
+++ php_website/index_en.php
@@ -5,21 +5,21 @@
     <meta content="width=device-width, initial-scale=1" name="viewport">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="../css/navbar.css">
-    <link rel="stylesheet" href="../css/book.css">
+    <link rel="stylesheet" href="../css/home.css">
     <link rel="stylesheet" href="../css/footer.css">
     <link rel="stylesheet" href="../css/main.css">
     <link rel="icon" type="image/png" href="../images/pinkoviicon.png">
-    <title>Book | Hotel PINKOVI Nessebar</title>
+    <title>Hotel Pinkovi - Nessebar</title>
 </head>
 <body>
     <header>
-        <div class="navbar_logo"><a href="#"><img src="../images/pinkovilogo.png"></a></div>
+        <div class="navbar_logo"><a href="#"><img src="../images/pinkovilogo-en.png"></a></div>
         <div id="hamburger_menu" class="hamburger_menu"><i class="fa-solid fa-bars"></i></div>
         <div id="navbar_buttons" class="navbar_buttons">
             <a href="#about"><button>ABOUT</button></a>
             <a href="#bistro"><button>BISTRO</button></a>
             <a href="#entertainment"><button>ENTERTAINMENT</button></a>
-            <a href="#location"><button>LOCATION AND TRANSPORT</button></a>
+            <a href="#location"><button>LOCATION</button></a>
             <a href="#"><button>GALLERY</button></a>
             <a href="#"><button>CONTACTS</button></a>
         </div>
@@ -32,116 +32,103 @@
         </div>
     </header>
 
-    <div class="prices">
-        <h1>Prices</h1>
-        <table>
-            <tbody>
-                <tr>
-                    <td colspan="2" rowspan="2" class="color">SEASON 2023</td>
-                    <td colspan="5" class="color">Dates</td>
-                </tr>
-                <tr>
-                    <td class="color2">01.06 - 15.06</td>
-                    <td class="color2">16.06 - 08.07</td>
-                    <td class="color2">09.07 - 25.08</td>
-                    <td class="color2">26.08 - 15.09</td>
-                    <td class="color2">16.09 - 30.09</td>
-                </tr>
-                <tr>
-                    <td rowspan="2" class="color shrink">Double room</td>
-                    <td class="color2">sea view</td>
-                    <td>100</td>
-                    <td>120</td>
-                    <td>145</td>
-                    <td>120</td>
-                    <td>100</td>
-                </tr>
-                <tr>
-                    <td class="color2">side view</td>
-                    <td>95</td>
-                    <td>115</td>
-                    <td>135</td>
-                    <td>115</td>
-                    <td>95</td>
-                </tr>
-                <tr>
-                    <td rowspan="2" class="color shrink">Double room with extra bed</td>
-                    <td class="color2">sea view</td>
-                    <td>125</td>
-                    <td>140</td>
-                    <td>170</td>
-                    <td>140</td>
-                    <td>125</td>
-                </tr>
-                <tr>
-                    <td class="color2">side view</td>
-                    <td>115</td>
-                    <td>130</td>
-                    <td>160</td>
-                    <td>130</td>
-                    <td>115</td>
-                </tr>
-                <tr>
-                    <td class="color shrink">Double room with two extra beds</td>
-                    <td class="color2">sea view</td>
-                    <td>140</td>
-                    <td>160</td>
-                    <td>185</td>
-                    <td>160</td>
-                    <td>140</td>
-                </tr>
-                <tr>
-                    <td class="color shrink">Apartment (2+2)</td>
-                    <td class="color2">sea view</td>
-                    <td>150</td>
-                    <td>200</td>
-                    <td>240</td>
-                    <td>200</td>
-                    <td>150</td>
-                </tr>
-                <tr>
-                    <td class="color shrink">Studio (2+2)</td>
-                    <td class="color2">sea view</td>
-                    <td>130</td>
-                    <td>170</td>
-                    <td>190</td>
-                    <td>170</td>
-                    <td>130</td>
-                </tr>
-            </tbody>
-        </table>
-        <h2>More than 7 nights – special discount of 5%</h2>
+    <div class="header_image">
+        <img class="header_image_logo" src="../images/pinkovilogowhite.png">
+        <div class="container">
+            <div class="wrapper">
+                <img src="../images/header.jpg">
+                <img src="../images/header2.jpg">
+                <img src="../images/header3.jpg">
+            </div>
+        </div>
     </div>
 
-    <div class="prices_text">
-        <div>
-            <ul>
-                <li>All prices are in Bulgarian "lev" (BGN) including VAT and tourist taxes</li>
-                <li>The prices listed above are for one night per one room, apartment or studio</li>
-                <li>Prices include breakfast</li>
-                <li>Prices include free:
-                  <ul>
-                    <li>sunbeds and parasols on the beach terrace</li>
-                    <li>parking lot just in front of the hotel</li>
-                    <li>wi-fi</li>
-                  </ul>
-                </li>
-                <li>For children up to 2 years - 100% free</li>
-                <li>Double room used as single – 80% of the price for double room</li>
-                <li>Check-in: after 14:00 | Check-out: until 12:00</li>
-                <li>Pets are not allowed in the hotel!</li>
-            </ul>
+    <div id="about" class="section about">
+        <div class="about_text">
+            <h1>ABOUT THE PINKOVI HOTEL</h1>
+            <p>Family Hotel PINKOVI is nestled in a vibrant and rapidly growing area of Nessebar, parallel to the beautiful beach coast. Its convenient location provides easy access to the main road connecting the popular tourist resorts of Nessebar and Ravda.</p>
+            <p>It boasts a prime location right by the slopes of a serene Black Sea beach, offering breathtaking panoramic views of the coastline from each of its 20 rooms.</p>
+            <p>Despite the growing interest in the region, Family Hotel PINKOVI offers a peaceful and serene environment, providing everything you need for a fantastic beach vacation.</p>
+        </div>
+        <div class="about_image">
+            <img src="../images/about_image.jpg">
+        </div>
+    </div>
 
-              
-            <p>To confirm your reservation you must pay a deposit of 20% of your total.</p>
+    <div id="features" class="features">
+        <ul>
+            <div>
+                <li><i class="fa-solid fa-bed"></i><p>Double rooms</p></li>
+                <li><i class="fa-solid fa-bed"></i><p>Double rooms with one extra bed</p></li>
+                <li><i class="fa-solid fa-bed"></i><p>Double rooms with two extra beds</p></li>
+                <li><i class="fa-solid fa-bed"></i><p>One apartment /2+2/</p></li>
+                <li><i class="fa-solid fa-bed"></i><p>Two garret studios with a kitchenette – bedroom with two extra beds</p></li>
+            </div>
+            <div>
+                <li><i class="fa-solid fa-wifi"></i><p>Free WiFi</p></li>
+                <li><i class="fa-solid fa-lock"></i><p>Safe (1.00 EUR/day)</p></li>
+                <li><i class="fa-solid fa-square-parking"></i><p>Free parking</p></li>
+                <li><i class="fa-solid fa-umbrella-beach"></i><p>A free sunbed and parasol on the terrace above the beach</p></li>
+                <li><i class="fa-solid fa-utensils"></i><p>A bistro</p></li>
+            </div>
+        </ul>
+    </div>
 
-            <p>Bank: ProCredit Bank<br>
-            BIC: PRCBBGSF<br>
-            IBAN: BG 07 PRCB 9230 1034 1675 14<br>
-            Pinkovi Ltd.</p>
+    <div id="bistro" class="section food">
+        <div class="section_images">
+            <div class="image">
+                <img src="../images/bistro_1.jpg">
+            </div>
+            <div class="image">
+                <img src="../images/bistro_2.jpg">
+            </div>
+            <div class="image">
+                <img src="../images/bistro_3.jpg">
+            </div>
+            <div class="image">
+                <img src="../images/bistro_4.jpg">
+            </div>
+        </div>
+        <div class="section_text">
+            <h1>BISTRO</h1>
+            <p>Bistro PINKOVI, situated on the hotel's ground floor, offers both outdoor and indoor seating.</p>
+            <p>It provides a cozy and serene setting for enjoying a cup of coffee, lunch, or dinner with friends.</p>
+            <p>Our menu features a selection of Bulgarian and European cuisine, with a focus on seafood dishes. While our main fish menu predominantly showcases Black Sea delicacies, we also serve fish from Greece, Turkey, Italy, France, and Norway.</p>
+        </div>
+    </div>
 
-            <p>Please note that your reservation will be considered valid only upon receiving confirmation from us via phone, email, or fax.</p>
-              
+    <div id="entertainment" class="section fun">
+        <div class="section_text">
+            <h1>ENTERTAINMENT</h1>
+            <p>Family Hotel PINKOVI is nestled along the picturesque and serene coastline of a beautiful bay. The beach provides a lifeguard, as well as parasols and lounges for your comfort. Additionally, you can enjoy a range of thrilling water activities such as banana boating, water wheels, water skiing, water surfing, wind surfing, and even take part in fitness and surfing lessons. When the sun sets, Nessebar comes alive with its vibrant nightlife, offering a plethora of clubs, restaurants, and bars to explore.</p>
+            <p>We also provide tourist information for various exciting events, tours, and excursions, as well as convenient options for car rentals, bicycle hires, and taxi services.</p>
+        </div>
+        <div class="section_images">
+            <div class="image">
+                <img src="../images/fun_1.jpg">
+            </div>
+            <div class="image">
+                <img src="../images/fun_2.jpg">
+            </div>
+            <div class="image">
+                <img src="../images/fun_3.jpg">
+            </div>
+            <div class="image">
+                <img src="../images/fun_4.jpg">
+            </div>
+        </div>
+    </div>
+
+    <div id="location" class="section location">
+        <div class="section_images">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5868.85510255236!2d27.6894789!3d42.6522943!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40a69f35a807ce65%3A0x5c96c3310d65a5d3!2z0KHQtdC80LXQtdC9INGF0L7RgtC10Lsg4oCc0J_QuNC90LrQvtCy0LjigJ0!5e0!3m2!1sbg!2sbg!4v1694526262572!5m2!1sbg!2sbg" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <div class="section_text">
+            <h1>LOCATION AND TRANSPORT</h1>
+            <p>BULGARIA, 8230 Nessebar, 24 “Aurelia” district, Family hotel PINKOVI</p>
+            <p>Family Hotel PINKOVI is conveniently situated just 1.8km from Ravda, 1.5km south of Nessebar, and only 5km from Sunny Beach. Adjacent to the hotel, you'll find a bus stop on the main road that connects Ravda and Nessebar, offering swift transportation to Bourgas, Nessebar, Sunny Beach, and Pomorie.</p>
+            <p>Bourgas Airport is approximately 15km away from the hotel.</p>
+            <p>Useful links: <a href="https://burgas-airport.bg/" target="_blank">Bourgas Airport</a> | <a href="https://varna-airport.bg/" target="_blank">Varna Airport</a> | <a href="https://bgrazpisanie.com/" target="_blank">BGRazpisanie.com</a> | <a href="https://transport.bgstart.net/" target="_blank">TransportBG.com</a> | <a href="https://www.bdz.bg/bg" target="_blank">Bulgarian Railways</a></p>
         </div>
     </div>
 
---
+++ php_website/prices_en.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="../css/navbar.css">
+    <link rel="stylesheet" href="../css/book.css">
+    <link rel="stylesheet" href="../css/footer.css">
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="icon" type="image/png" href="../images/pinkoviicon.png">
+    <title>Book | Hotel PINKOVI Nessebar</title>
+</head>
+<body>
+    <header>
+        <div class="navbar_logo"><a href="#"><img src="../images/pinkovilogo.png"></a></div>
+        <div id="hamburger_menu" class="hamburger_menu"><i class="fa-solid fa-bars"></i></div>
+        <div id="navbar_buttons" class="navbar_buttons">
+            <a href="#about"><button>ABOUT</button></a>
+            <a href="#bistro"><button>BISTRO</button></a>
+            <a href="#entertainment"><button>ENTERTAINMENT</button></a>
+            <a href="#location"><button>LOCATION AND TRANSPORT</button></a>
+            <a href="#"><button>GALLERY</button></a>
+            <a href="#"><button>CONTACTS</button></a>
+        </div>
+        <div class="right">
+            <div class="navbar_languages">
+                <a href="#"><img src="../images/bulgarian.png"></a>
+                <a href="#"><img src="../images/english.png"></a>
+            </div>
+            <div class="navbar_book"><button>PRICES</button></div>
+        </div>
+    </header>
+
+    <div class="prices">
+        <h1>Prices</h1>
+        <table>
+            <tbody>
+                <tr>
+                    <td colspan="2" rowspan="2" class="color">SEASON 2023</td>
+                    <td colspan="5" class="color">Dates</td>
+                </tr>
+                <tr>
+                    <td class="color2">01.06 - 15.06</td>
+                    <td class="color2">16.06 - 08.07</td>
+                    <td class="color2">09.07 - 25.08</td>
+                    <td class="color2">26.08 - 15.09</td>
+                    <td class="color2">16.09 - 30.09</td>
+                </tr>
+                <tr>
+                    <td rowspan="2" class="color shrink">Double room</td>
+                    <td class="color2">sea view</td>
+                    <td>100</td>
+                    <td>120</td>
+                    <td>145</td>
+                    <td>120</td>
+                    <td>100</td>
+                </tr>
+                <tr>
+                    <td class="color2">side view</td>
+                    <td>95</td>
+                    <td>115</td>
+                    <td>135</td>
+                    <td>115</td>
+                    <td>95</td>
+                </tr>
+                <tr>
+                    <td rowspan="2" class="color shrink">Double room with extra bed</td>
+                    <td class="color2">sea view</td>
+                    <td>125</td>
+                    <td>140</td>
+                    <td>170</td>
+                    <td>140</td>
+                    <td>125</td>
+                </tr>
+                <tr>
+                    <td class="color2">side view</td>
+                    <td>115</td>
+                    <td>130</td>
+                    <td>160</td>
+                    <td>130</td>
+                    <td>115</td>
+                </tr>
+                <tr>
+                    <td class="color shrink">Double room with two extra beds</td>
+                    <td class="color2">sea view</td>
+                    <td>140</td>
+                    <td>160</td>
+                    <td>185</td>
+                    <td>160</td>
+                    <td>140</td>
+                </tr>
+                <tr>
+                    <td class="color shrink">Apartment (2+2)</td>
+                    <td class="color2">sea view</td>
+                    <td>150</td>
+                    <td>200</td>
+                    <td>240</td>
+                    <td>200</td>
+                    <td>150</td>
+                </tr>
+                <tr>
+                    <td class="color shrink">Studio (2+2)</td>
+                    <td class="color2">sea view</td>
+                    <td>130</td>
+                    <td>170</td>
+                    <td>190</td>
+                    <td>170</td>
+                    <td>130</td>
+                </tr>
+            </tbody>
+        </table>
+        <h2>More than 7 nights – special discount of 5%</h2>
+    </div>
+
+    <div class="prices_text">
+        <div>
+            <ul>
+                <li>All prices are in Bulgarian "lev" (BGN) including VAT and tourist taxes</li>
+                <li>The prices listed above are for one night per one room, apartment or studio</li>
+                <li>Prices include breakfast</li>
+                <li>Prices include free:
+                  <ul>
+                    <li>sunbeds and parasols on the beach terrace</li>
+                    <li>parking lot just in front of the hotel</li>
+                    <li>wi-fi</li>
+                  </ul>
+                </li>
+                <li>For children up to 2 years - 100% free</li>
+                <li>Double room used as single – 80% of the price for double room</li>
+                <li>Check-in: after 14:00 | Check-out: until 12:00</li>
+                <li>Pets are not allowed in the hotel!</li>
+            </ul>
+
+              
+            <p>To confirm your reservation you must pay a deposit of 20% of your total.</p>
+
+            <p>Bank: ProCredit Bank<br>
+            BIC: PRCBBGSF<br>
+            IBAN: BG 07 PRCB 9230 1034 1675 14<br>
+            Pinkovi Ltd.</p>
+
+            <p>Please note that your reservation will be considered valid only upon receiving confirmation from us via phone, email, or fax.</p>
+              
+        </div>
+    </div>
+
+    <footer>
+        <a href="mailto:pinkovh@abv.bg"><i class="fa-solid fa-envelope"></i></a>
+        <a href="tel:+359894737399"><i class="fa-solid fa-phone"></i></a>
+        <a href="https://www.facebook.com/familyhotelpinkovi/?ref=embed_page" target="_blank"><i class="fa-brands fa-square-facebook"></i></a>
+        <a href="https://goo.gl/maps/AfRPjQWB6pB1MDTz7" target="_blank"><i class="fa-solid fa-location-dot"></i></a>
+    </footer>
+</body>
+</html>
